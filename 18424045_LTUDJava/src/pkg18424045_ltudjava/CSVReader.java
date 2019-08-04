77b3e3456703d7ccaa46e4db20794bbc8e3549da
nguyetpa <nguyetpham180397@gmail.com>
2019-08-04 21:22:16 +0700

    no message

--- 18424045_LTUDJava/src/pkg18424045_ltudjava/CSVReader.java
+++ 18424045_LTUDJava/src/pkg18424045_ltudjava/CSVReader.java
@@ -3,12 +3,58 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pkg18424045_ltudjava;
+package pkg18424045_btth01;
+
+import Entities.Account;
+import Entities.Score;
+import Entities.Student;
+import Entities.Subject;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
  * @author LAP11368-local
  */
-public class CSVReader {
-    
+public class CSVReader <T> {
+    public static final String COMMA_DELIMITER = ",";
+    public List<T> readCSV(String fileName, T type){
+        List<List<String>> records = new ArrayList<List<String>>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))){
+            String line;
+            while ((line = br.readLine()) != null){
+                String[] values = line.split(COMMA_DELIMITER);
+                records.add(Arrays.asList(values));
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+    }
+        List<T> result = new ArrayList<T>();
+         for (int i = 0; i < records.size(); i++){
+            List<String> metaData = records.get(i);
+            Object item = null;
+            if (type instanceof Student){
+                item = Student.readFromMetaData(metaData);
+            }
+            
+            if (type instanceof Account){
+                item = Account.readFromMetaData(metaData);
+            }
+            
+            if (type instanceof Score){
+                item = Score.readFromMetaData(metaData);
+            }
+            
+            if (type instanceof Subject){
+                item = Subject.readFromMetaData(metaData);
+            }
+            result.add((T) item);
+        }
+        return result;
+    }
 }
